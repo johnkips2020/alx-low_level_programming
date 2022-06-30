625fe76dc9f1d472df298d87f057954fd603208f
John Kipngeno <johnkips2002@gmail.com>
2022-06-30 16:39:13 +0300

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+*main-call printf and prints with proper grammar, but the outcome is a piece of art,
+*return:0 terminates the program
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

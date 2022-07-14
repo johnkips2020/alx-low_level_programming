24dba326920c437f7b45a086fc3c4c85d6e800ae
John Kipngeno <johnkips2002@gmail.com>
2022-07-14 22:52:10 +0300

    pointers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,16 +1,12 @@
 #include "main.h"
-
+#include <stdio.h>
 
 /**
-*main -adds two numbers
-*
-*
-*Return: 0 always success
-*/
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_buffer(char *b, int size)
 {
-	int n1;
-	int n2;
-	int r;
+	
 }

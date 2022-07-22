52c1cbf40fd6b7fdaafebd2aaf371710464b0ce5
John Kipngeno <johnkips2002@gmail.com>
2022-07-22 13:43:14 +0300

    static libraries

---
+++ 0x09-static_libraries/9-memcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates n bytes from a string to another
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes of str to concatenate
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+}
